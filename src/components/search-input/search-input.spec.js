80e2f5f126fb271a61e9fb9a6e6db3541421ef4a
rushian_san <luciano.ivec@gmail.com>
2022-05-10 18:51:11 -0300

    primeiros testes

---
+++ src/components/search-input/search-input.spec.js
@@ -0,0 +1,16 @@
+import { mount } from '@vue/test-utils'
+import SearchInput from '../index.vue'
+
+describe('search-input - Unit', () => {
+    it('should be a vue instance', () => {
+        const wrapper = mount(SearchInput,{
+          propsData: {
+            value: ''
+          }
+        })
+      
+        expect(wrapper.vm).toBeDefined()
+      })
+      
+
+})
